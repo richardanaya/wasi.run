62d18dfd55c1fe8ae51e21e60e8b246e6675c8d4
Richard Anaya <richard.anaya@gmail.com>
2022-08-18 19:23:34 -0700

    deploy

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import { Terminal } from "xterm";
+import { FitAddon } from 'xterm-addon-fit';
+
+
 
 const params = new URLSearchParams(window.location.search);
 const app = params.get("app");
@@ -6,9 +9,15 @@ const app = params.get("app");
 if (app !== null) {
   window.document.body.innerHTML = `<div id="terminal"></div>`;
   const terminalElement = document.getElementById("terminal");
+  document.body.style.padding = "0";
+  document.body.style.margin = "0";
+  document.body.style.backgroundColor = "#000";
   if (terminalElement) {
-    const term = new Terminal();
-    term.open(terminalElement);
-    term.write("Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ");
+    const terminal = new Terminal();
+    const fitAddon = new FitAddon();
+    terminal.loadAddon(fitAddon);
+    terminal.open(document.querySelector("html")!);
+    terminal.write("Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ");
+    fitAddon.fit();
   }
 }
