b623c7ab3e185334656f139299215baf1f13489c
Richard Anaya <richard.anaya@gmail.com>
2022-08-18 19:45:05 -0700

    deploy

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,39 @@
+// @ts-ignore-next-line
+import { init, WASI } from '@wasmer/wasi'; 
+
 import { Terminal } from "xterm";
 import { FitAddon } from 'xterm-addon-fit';
-
+import { Buffer } from 'buffer'
+(globalThis as any).Buffer = Buffer
 
 
 const params = new URLSearchParams(window.location.search);
 const app = params.get("app");
+
+async function start(terminal:Terminal){
+  await init();
+
+  const wasi = new WASI({
+    env: {
+        // 'ENVVAR1': '1',
+        // 'ENVVAR2': '2'
+    },
+    args: [
+        // 'command', 'arg1', 'arg2'
+    ],
+  });
+  
+  const moduleBytes = fetch("https://deno.land/x/wasm/tests/demo.wasm");
+  const module = await WebAssembly.compileStreaming(moduleBytes);
+  // Instantiate the WASI module
+  await wasi.instantiate(module, {});
+  
+  // Run the start function
+  const exitCode = wasi.start();
+  const stdout = wasi.getStdoutString();
+  
+  terminal.write(`${stdout}(exit code: ${exitCode})`);
+}
 
 if (app !== null) {
   window.document.body.innerHTML = `<div id="terminal"></div>`;
@@ -17,8 +46,8 @@ if (app !== null) {
     const fitAddon = new FitAddon();
     terminal.loadAddon(fitAddon);
     terminal.open(document.querySelector("html")!);
-    terminal.write("Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ");
     fitAddon.fit();
+    start(terminal)
   }
 } else {
   window.document.body.innerHTML = `
