84ff2c66ae29781fc054bfc213c0852a69e39625
Richard Anaya <richard.anaya@gmail.com>
2022-08-30 20:51:16 -0700

    deploy

--- examples/wasi_hello_world/src/main.rs
+++ examples/wasi_hello_world/src/main.rs
@@ -1,3 +1,20 @@
+use kimono::*;
+
+const STYLE: Style = Style::new()
+    .padding_top(1)
+    .padding_left(1)
+    .padding_right(2)
+    .padding_bottom(3)
+    .border(1)
+    .border_style(BORDER_STYLE_OUTLINE)
+    .border_color(0xddae74)
+    .border_background(0xbc5633)
+    .color(0xebdeb8)
+    .background(0x407955);
+
 fn main() {
-    println!("Hello, world WASI!");
+    colored::control::set_override(true);
+    clear_screen();
+    STYLE.render_at_position(10, 3, "着物");
+    print!("\n\r");
 }
